6005a6cdfa2b0273adfce0fb427b828b06d3e68a
Ryan Furrer <ryandotfurrer@gmail.com>
2025-03-21 22:41:11 -0400

    Add remote image pattern for GitHub in Next.js config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'raw.githubusercontent.com',
+      },
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
